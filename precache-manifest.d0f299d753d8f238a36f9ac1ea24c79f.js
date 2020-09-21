745a8bc2242c6e19fe26a61aa049515c6e5f4259
Matthew Gutierrez <matthew24.gutierrez@gmail.com>
2020-09-20 19:37:18 -0700

    Updates

---
+++ precache-manifest.d0f299d753d8f238a36f9ac1ea24c79f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032d001973f8b88e469ace2c44b64a0d",
+    "url": "/clarification-converter/index.html"
+  },
+  {
+    "revision": "ad5160ff3b9bd15daba3",
+    "url": "/clarification-converter/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "107eab9b2413fcda3593",
+    "url": "/clarification-converter/static/css/main.357ae991.chunk.css"
+  },
+  {
+    "revision": "ad5160ff3b9bd15daba3",
+    "url": "/clarification-converter/static/js/2.3ff76db7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/clarification-converter/static/js/2.3ff76db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107eab9b2413fcda3593",
+    "url": "/clarification-converter/static/js/main.9b999000.chunk.js"
+  },
+  {
+    "revision": "dcbb005b13ec160c3659",
+    "url": "/clarification-converter/static/js/runtime-main.8fe64e0a.js"
+  }
+]);
