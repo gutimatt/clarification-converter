52acb5fb2fc93ddab1490073df9929269b5aa0c8
Matthew Gutierrez <matthew24.gutierrez@gmail.com>
2020-12-14 08:33:32 -0700

    Updates

--- precache-manifest.e5b92a921cc5171145c403d5680a342f.js
+++ precache-manifest.e5b92a921cc5171145c403d5680a342f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2cf5cb163e7d3471a1a126d6087c3589",
+    "revision": "0feeef3dc00a78efa69e49852d14e13b",
     "url": "/clarification-converter/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/clarification-converter/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "4b67d9075ecf2481839a",
+    "revision": "53a139406d0e0e6e9a3b",
     "url": "/clarification-converter/static/css/main.267e44ec.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/clarification-converter/static/js/2.8e70c1f2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4b67d9075ecf2481839a",
-    "url": "/clarification-converter/static/js/main.3ab50610.chunk.js"
+    "revision": "53a139406d0e0e6e9a3b",
+    "url": "/clarification-converter/static/js/main.a6179633.chunk.js"
   },
   {
     "revision": "dcbb005b13ec160c3659",
